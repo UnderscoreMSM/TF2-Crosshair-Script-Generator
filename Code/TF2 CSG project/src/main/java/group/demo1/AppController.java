fe2d40d610396166df56f1524fc91c2df5111b62
MSM <msm15x@outlook.com>
2022-03-26 17:56:34 +0300

    Fixed script generation
    Was broken after the last implementation.

--- Code/TF2 CSG project/src/main/java/group/demo1/AppController.java
+++ Code/TF2 CSG project/src/main/java/group/demo1/AppController.java
@@ -100,148 +100,143 @@ public class AppController {
     ObservableList<String> list = FXCollections.observableList(Arrays.asList(crosshairFolder.list(filter)));
 
     @FXML
-    protected void onGenerateClick() {
-        try {
-            String targetDir = tfDirectory.getText();
-            String scriptsDir = targetDir + "/Custom crosshairs/scripts";
-            String destination = targetDir + "/Custom crosshairs/materials/vgui/replay/thumbnails";
-            File folder = new File(targetDir + "/Custom crosshairs");
-            File scripts = new File(targetDir + "/Custom crosshairs/scripts");
-            File crosshairs = new File(targetDir + "/Custom crosshairs/materials");
-            File vguiDir = new File(targetDir + "/Custom crosshairs/materials/vgui");
-            File replayDir = new File(targetDir + "/Custom crosshairs/materials/vgui/replay");
-            File thumbnailsDir = new File(destination);
+    protected void onGenerateClick() throws IOException {
+        String targetDir = tfDirectory.getText();
+        String scriptsDir = targetDir + "/Custom crosshairs/scripts";
+        String destination = targetDir + "/Custom crosshairs/materials/vgui/replay/thumbnails";
+        File folder = new File(targetDir + "/Custom crosshairs");
+        File scripts = new File(targetDir + "/Custom crosshairs/scripts");
+        File crosshairs = new File(targetDir + "/Custom crosshairs/materials");
+        File vguiDir = new File(targetDir + "/Custom crosshairs/materials/vgui");
+        File replayDir = new File(targetDir + "/Custom crosshairs/materials/vgui/replay");
+        File thumbnailsDir = new File(destination);
 
-            boolean boolFolder = folder.mkdir();
-            boolean boolScripts = scripts.mkdir();
-            boolean boolCrosshairs = crosshairs.mkdir();
-            boolean boolVgui = vguiDir.mkdir();
-            boolean boolReplay = replayDir.mkdir();
-            boolean boolthumb = thumbnailsDir.mkdir();
+        boolean boolFolder = folder.mkdir();
+        boolean boolScripts = scripts.mkdir();
+        boolean boolCrosshairs = crosshairs.mkdir();
+        boolean boolVgui = vguiDir.mkdir();
+        boolean boolReplay = replayDir.mkdir();
+        boolean boolthumb = thumbnailsDir.mkdir();
 
-            if (boolFolder) { // TODO Clean this up
-                System.out.println("Folder created!");
-                if (boolScripts) {
-                    System.out.println("scripts folder created!");
-                }
-                if (boolCrosshairs) {
-                    System.out.println("crosshairs folder created!");
-                    if (boolVgui) {
-                        System.out.println("vgui folder created!");
-                        if (boolReplay) {
-                            System.out.println("replay folder created!");
-                            if (boolthumb) {
-                                System.out.println("thumbnails folder created!");
-                                System.out.println("All necessary folders have been created!");
-                            }
+        if (boolFolder) { // TODO Clean this up
+            System.out.println("Folder created!");
+            if (boolScripts) {
+                System.out.println("scripts folder created!");
+            }
+            if (boolCrosshairs) {
+                System.out.println("crosshairs folder created!");
+                if (boolVgui) {
+                    System.out.println("vgui folder created!");
+                    if (boolReplay) {
+                        System.out.println("replay folder created!");
+                        if (boolthumb) {
+                            System.out.println("thumbnails folder created!");
+                            System.out.println("All necessary folders have been created!");
                         }
                     }
                 }
             }
+        }
 
-            if (!chkBoxNoScout.isSelected()) {
-                generate(cbScattergun, "tf_weapon_scattergun.txt", scriptsDir);
-                generate(cbSodaPopper, "tf_weapon_soda_popper.txt", scriptsDir);
-                generate(cbShortstop, "tf_weapon_handgun_scout_primary.txt", scriptsDir);
-                generate(cbBabyFaceBlaster, "tf_weapon_pep_brawler_blaster.txt", scriptsDir);
-                generate(cbScoutPistol, "tf_weapon_pistol_scout.txt", scriptsDir);
-                generate(cbWinger, "tf_weapon_handgun_scout_secondary.txt", scriptsDir);
-                generate(cbClassic, "tf_weapon_cleaver.txt", scriptsDir);
-                generate(cbBonkDrink, "tf_weapon_lunchbox_drink.txt", scriptsDir);
-                generate(cbMadMilk, "tf_weapon_jar_milk.txt", scriptsDir);
-                generate(cbBat, "tf_weapon_bat.txt", scriptsDir);
-                generate(cbFishMelee, "tf_weapon_bat_fish.txt", scriptsDir);
-                generate(cbSandman, "tf_weapon_bat_wood.txt", scriptsDir);
-                generate(cbWrapAssassin, "tf_weapon_bat_giftwarp.txt", scriptsDir);
-            }
-            if (!chkBoxNoSoldier.isSelected()) {
-                generate(cbRocketLauncher, "tf_weapon_rocketlauncher.txt", scriptsDir);
-                generate(cbDirectHit, "tf_weapon_rocketlauncher_directhit.txt", scriptsDir);
-                generate(cbMangler, "tf_weapon_particle_cannon.txt", scriptsDir);
-                generate(cbAirStrike, "tf_weapon_rocketlauncher_airstrike.txt", scriptsDir);
-                generate(cbSoldierSG, "tf_weapon_shotgun_soldier.txt", scriptsDir);
-                generate(cbBuffs, "tf_weapon_buff_item.txt", scriptsDir);
-                generate(cbBison, "tf_weapon_raygun.txt", scriptsDir);
-                generate(cbShovel, "tf_weapon_shovel.txt", scriptsDir);
-                generate(cbSoldierKatana, "tf_weapon_katana.txt", scriptsDir);
-            }
-            if (!chkBoxNoPyro.isSelected()) {
-                generate(cbFlameThrower, "tf_weapon_flamethrower.txt", scriptsDir);
-                generate(cbDragonFury, "tf_weapon_rocketlauncher_fireball.txt", scriptsDir);
-                generate(cbPyroSG, "tf_weapon_shotgun_pyro.txt", scriptsDir);
-                generate(cbFlareGun, "tf_weapon_flaregun.txt", scriptsDir);
-                generate(cbManmelter, "tf_weapon_flaregun_revenge.txt", scriptsDir);
-                generate(cbThrusters, "tf_weapon_rocketpack.txt", scriptsDir);
-                generate(cbGasPasser, "tf_weapon_jar.txt", scriptsDir);
-                generate(cbAxe, "tf_weapon_fireaxe.txt", scriptsDir);
-                generate(cbHotHand, "tf_weapon_slap.txt", scriptsDir);
-            }
-            if (!chkBoxNoDemoman.isSelected()) {
-                generate(cbGrenadeLauncher, "tf_weapon_grenadelauncher.txt", scriptsDir);
-                generate(cbCannon, "tf_weapon_cannon.txt", scriptsDir);
-                generate(cbSticky, "tf_weapon_pipebomblauncher.txt", scriptsDir);
-                generate(cbBottle, "tf_weapon_bottle.txt", scriptsDir);
-                generate(cbSwords, "tf_weapon_sword.txt", scriptsDir);
-                generate(cbCaber, "tf_weapon_stickbomb.txt", scriptsDir);
-//            generate(cbDemoKatana, "tf_weapon_katana.txt", scriptsDir);
-            }
-            if (!chkBoxNoHeavy.isSelected()) {
-                generate(cbMiniGun, "tf_weapon_minigun.txt",  scriptsDir);
-                generate(cbHeavySG, "tf_weapon_shotgun_hwg.txt", scriptsDir);
-                generate(cbFood, "tf_weapon_lunchbox.txt", scriptsDir);
-                generate(cbFists, "tf_weapon_fists.txt", scriptsDir);
-            }
-            if(!chkBoxNoEngineer.isSelected()) {
-                generate(cbEngiSG, "tf_weapon_shotgun_primary.txt", scriptsDir);
-                generate(cbPomson, "tf_weapon_drg_pompson.txt", scriptsDir);
-                generate(cbRescueRanger, "tf_weapon_shotgun_building_rescue.txt", scriptsDir);
-                generate(cbFrontierJustice, "tf_weapon_sentry_revenge.txt", scriptsDir);
-                generate(cbEngiPistol, "tf_weapon_pistol.txt", scriptsDir);
-                generate(cbWrangler, "tf_weapon_laser_pointer.txt", scriptsDir);
-                generate(cbShortCircuit, "tf_weapon_mechanical_arm.txt", scriptsDir);
-                generate(cbWrench, "tf_weapon_wrench.txt", scriptsDir);
-                generate(cbGunslinger, "tf_weapon_robot_arm.txt", scriptsDir);
-            }
-            if(!chkBoxNoMedic.isSelected()) {
-                generate(cbSyringeGun, "tf_weapon_syringegun_medic.txt", scriptsDir);
-                generate(cbCrossbow, "tf_weapon_crossbow.txt", scriptsDir);
-                generate(cbMediGun, "tf_weapon_medigun.txt", scriptsDir);
-                generate(cbBonesaw, "tf_weapon_bonesaw.txt", scriptsDir);
-            }
-            if (!chkBoxNoSniper.isSelected()) {
-                generate(cbSniperRifle, "tf_weapon_sniperrifle.txt", scriptsDir);
-                generate(cbHuntsman, "tf_weapon_compound_bow.txt", scriptsDir);
-                generate(cbHitman, "tf_weapon_sniperrifle_decap.txt", scriptsDir);
-                generate(cbClassic, "tf_weapon_sniperrifle_classic.txt", scriptsDir);
-                generate(cbSMG, "tf_weapon_smg.txt", scriptsDir);
-                generate(cbCarbine, "tf_weapon_charged_smg.txt", scriptsDir);
-//            generate(cbJarate, "tf_weapon_jar.txt", scriptsDir);
-                generate(cbKukri, "tf_weapon_club.txt", scriptsDir);
-            }
-            if (!chkBoxNoSpy.isSelected()) {
-                generate(cbRevolver, "tf_weapon_revolver.txt", scriptsDir);
-                generate(cbKnife, "tf_weapon_knife.txt", scriptsDir);
-                generate(cbSapper, "tf_weapon_sapper.txt", scriptsDir);
-            }
+        if (!chkBoxNoScout.isSelected()) {
+            generate(cbScattergun, "tf_weapon_scattergun.txt", scripts);
+            generate(cbSodaPopper, "tf_weapon_soda_popper.txt", scripts);
+            generate(cbShortstop, "tf_weapon_handgun_scout_primary.txt", scripts);
+            generate(cbBabyFaceBlaster, "tf_weapon_pep_brawler_blaster.txt", scripts);
+            generate(cbScoutPistol, "tf_weapon_pistol_scout.txt", scripts);
+            generate(cbWinger, "tf_weapon_handgun_scout_secondary.txt", scripts);
+            generate(cbClassic, "tf_weapon_cleaver.txt", scripts);
+            generate(cbBonkDrink, "tf_weapon_lunchbox_drink.txt", scripts);
+            generate(cbMadMilk, "tf_weapon_jar_milk.txt", scripts);
+            generate(cbBat, "tf_weapon_bat.txt", scripts);
+            generate(cbFishMelee, "tf_weapon_bat_fish.txt", scripts);
+            generate(cbSandman, "tf_weapon_bat_wood.txt", scripts);
+            generate(cbWrapAssassin, "tf_weapon_bat_giftwrap.txt", scripts);
+        }
+        if (!chkBoxNoSoldier.isSelected()) {
+            generate(cbRocketLauncher, "tf_weapon_rocketlauncher.txt", scripts);
+            generate(cbDirectHit, "tf_weapon_rocketlauncher_directhit.txt", scripts);
+            generate(cbMangler, "tf_weapon_particle_cannon.txt", scripts);
+            generate(cbAirStrike, "tf_weapon_rocketlauncher_airstrike.txt", scripts);
+            generate(cbSoldierSG, "tf_weapon_shotgun_soldier.txt", scripts);
+            generate(cbBuffs, "tf_weapon_buff_item.txt", scripts);
+            generate(cbBison, "tf_weapon_raygun.txt", scripts);
+            generate(cbShovel, "tf_weapon_shovel.txt", scripts);
+            generate(cbSoldierKatana, "tf_weapon_katana.txt", scripts);
+        }
+        if (!chkBoxNoPyro.isSelected()) {
+            generate(cbFlameThrower, "tf_weapon_flamethrower.txt", scripts);
+            generate(cbDragonFury, "tf_weapon_rocketlauncher_fireball.txt", scripts);
+            generate(cbPyroSG, "tf_weapon_shotgun_pyro.txt", scripts);
+            generate(cbFlareGun, "tf_weapon_flaregun.txt", scripts);
+            generate(cbManmelter, "tf_weapon_flaregun_revenge.txt", scripts);
+            generate(cbThrusters, "tf_weapon_rocketpack.txt", scripts);
+            generate(cbGasPasser, "tf_weapon_jar.txt", scripts);
+            generate(cbAxe, "tf_weapon_fireaxe.txt", scripts);
+            generate(cbHotHand, "tf_weapon_slap.txt", scripts);
+        }
+        if (!chkBoxNoDemoman.isSelected()) {
+            generate(cbGrenadeLauncher, "tf_weapon_grenadelauncher.txt", scripts);
+            generate(cbCannon, "tf_weapon_cannon.txt", scripts);
+            generate(cbSticky, "tf_weapon_pipebomblauncher.txt", scripts);
+            generate(cbBottle, "tf_weapon_bottle.txt", scripts);
+            generate(cbSwords, "tf_weapon_sword.txt", scripts);
+            generate(cbCaber, "tf_weapon_stickbomb.txt", scripts);
+            generate(cbDemoKatana, "tf_weapon_katana.txt", scripts);
+        }
+        if (!chkBoxNoHeavy.isSelected()) {
+            generate(cbMiniGun, "tf_weapon_minigun.txt",  scripts);
+            generate(cbHeavySG, "tf_weapon_shotgun_hwg.txt", scripts);
+            generate(cbFood, "tf_weapon_lunchbox.txt", scripts);
+            generate(cbFists, "tf_weapon_fists.txt", scripts);
+        }
+        if(!chkBoxNoEngineer.isSelected()) {
+            generate(cbEngiSG, "tf_weapon_shotgun_primary.txt", scripts);
+            generate(cbPomson, "tf_weapon_drg_pomson.txt", scripts);
+            generate(cbRescueRanger, "tf_weapon_shotgun_building_rescue.txt", scripts);
+            generate(cbFrontierJustice, "tf_weapon_sentry_revenge.txt", scripts);
+            generate(cbEngiPistol, "tf_weapon_pistol.txt", scripts);
+            generate(cbWrangler, "tf_weapon_laser_pointer.txt", scripts);
+            generate(cbShortCircuit, "tf_weapon_mechanical_arm.txt", scripts);
+            generate(cbWrench, "tf_weapon_wrench.txt", scripts);
+            generate(cbGunslinger, "tf_weapon_robot_arm.txt", scripts);
+        }
+        if(!chkBoxNoMedic.isSelected()) {
+            generate(cbSyringeGun, "tf_weapon_syringegun_medic.txt", scripts);
+            generate(cbCrossbow, "tf_weapon_crossbow.txt", scripts);
+            generate(cbMediGun, "tf_weapon_medigun.txt", scripts);
+            generate(cbBonesaw, "tf_weapon_bonesaw.txt", scripts);
+        }
+        if (!chkBoxNoSniper.isSelected()) {
+            generate(cbSniperRifle, "tf_weapon_sniperrifle.txt", scripts);
+            generate(cbHuntsman, "tf_weapon_compound_bow.txt", scripts);
+            generate(cbHitman, "tf_weapon_sniperrifle_decap.txt", scripts);
+            generate(cbClassic, "tf_weapon_sniperrifle_classic.txt", scripts);
+            generate(cbSMG, "tf_weapon_smg.txt", scripts);
+            generate(cbCarbine, "tf_weapon_charged_smg.txt", scripts);
+            generate(cbJarate, "tf_weapon_jar.txt", scripts);
+            generate(cbKukri, "tf_weapon_club.txt", scripts);
+        }
+        if (!chkBoxNoSpy.isSelected()) {
+            generate(cbRevolver, "tf_weapon_revolver.txt", scripts);
+            generate(cbKnife, "tf_weapon_knife.txt", scripts);
+            generate(cbSapper, "tf_weapon_sapper.txt", scripts);
+        }
 
-            System.out.println("The following files will be copied:\n" + crosshairsGenerate);
+        System.out.println("The following files will be copied:\n" + crosshairsGenerate);
 
-            for (File e : crosshairsGenerate) { // Copies crosshair files to the generation path
-                String src = "crosshairs/" + e.getName();
-                String dest = destination + "/" + e.getName();
-                System.out.println("Copying from " + src + " to " + dest);
-                Files.copy(Path.of(src), Path.of(dest));
-                System.out.println("Copying complete!");
-            }
-        } catch (Exception e) {
-            System.out.println("Failed to generate; invalid path");
+        for (File e : crosshairsGenerate) { // Copies crosshair files to the generation path
+            String src = "crosshairs/" + e.getName();
+            String dest = destination + "/" + e.getName();
+            System.out.println("Copying from " + src + " to " + dest);
+            Files.copy(Path.of(src), Path.of(dest));
+            System.out.println("Copying complete!");
         }
     }
 
-    public void generate(ChoiceBox<String> cbWeapon, String weaponTXT, String folderDir) throws IOException {
-        addFileToList(removeExtension(cbWeapon.getValue()));
-        FileWriter script = new FileWriter(folderDir + "/" + weaponTXT);
-//        script.write(createScript(removeExtension(cbWeapon.getValue()) + ".vmt")); // Assuming the VMT file exists
+    public void generate(ChoiceBox<String> cbWeapon, String weaponTXT, File folderDir) throws IOException {
+        String crosshair = removeExtension(cbWeapon.getValue());
+        FileWriter script = new FileWriter(folderDir.getPath() + "/" + weaponTXT);
         File template = new File("templates/" + weaponTXT);
         String contents = "";
         Scanner scanner = new Scanner(template);
@@ -249,13 +244,12 @@ public class AppController {
         while (scanner.hasNextLine()) {
             contents = contents.concat(scanner.nextLine() + "\n");
         }
-        System.out.println("***** FILE CONTENTS *****");
-        System.out.println(contents);
-        System.out.println("*************************");
+        scanner.close();
 
-        contents = contents.replace("vgui/replay/thumbnails/", "vgui/replay/thumbnails/" + removeExtension(cbWeapon.getValue()));
+        contents = contents.replace("vgui/replay/thumbnails/", "vgui/replay/thumbnails/" + crosshair);
         script.write(contents);
         script.close();
+        addFileToList(crosshair);
     }
 
     @FXML
@@ -273,6 +267,7 @@ public class AppController {
         vmtDialog.getExtensionFilters().add(new FileChooser.ExtensionFilter("VMT Files (*.vmt)", "*.vmt"));
         File tempVTF = vtfDialog.showOpenDialog(null);
 
+        // TODO Properely handle the exception when cancelling the operation
         if (tempVTF != null) { // Will not show the VMT dialog if the user cancels half-way through the operation
             File tempVMT = vmtDialog.showOpenDialog(null);
             if (tempVMT != null && removeExtension(tempVTF.getName()).equals(removeExtension(tempVMT.getName()))) {
@@ -381,32 +376,7 @@ public class AppController {
         }
     }
 
-
-    // TODO Read from the VTF header to obtain the size of the selected crosshair
-    public String createScript(String vmtFile) { // Assuming the size is 64x64, for now
-        String template = """
-                WeaponData
-                {
-                    TextureData
-                    {
-                        "crosshair"
-                        {
-                            "file"          "vgui/replay/thumbnails/""";
-        template = template.concat(removeExtension(vmtFile) + """
-                "
-                            "x"		        "0"
-                            "y"		        "0"
-                            "width"		    "64"
-                            "height"	    "64"
-                        }
-                    }
-                }""");
-
-
-        return template;
-    }
-
-    public void addFileToList(String crosshairName) {
+    public void addFileToList(String crosshairName) { // Eleminates duplicate copies
         File vtfTemp = new File("crosshairs/" + crosshairName + ".vtf");
         File vmtTemp = new File("crosshairs/" + crosshairName + ".vmt");
         if (vtfTemp.exists() && vmtTemp.exists()) {
