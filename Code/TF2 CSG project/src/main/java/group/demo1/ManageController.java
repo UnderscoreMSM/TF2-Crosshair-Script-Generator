67d41ba3389bafcce371ba1432ab8468de3a7c83
MSM <msm15x@outlook.com>
2022-03-30 01:11:31 +0300

    Ready for beta release!
    All core features work.

--- Code/TF2 CSG project/src/main/java/group/demo1/ManageController.java
+++ Code/TF2 CSG project/src/main/java/group/demo1/ManageController.java
@@ -3,8 +3,7 @@ package group.demo1;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableView;
-
+import javafx.scene.control.ListView;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.Arrays;
@@ -15,9 +14,28 @@ public class ManageController {
     ObservableList<String> list = FXCollections.observableList(Arrays.asList(crosshairFolder.list(filter)));
 
     @FXML
-    private TableView<File[]> tableCrosshair;
+    private ListView<String> lvCrosshairs = new ListView<>();
 
     public void initialize() {
-        tableCrosshair.getItems().add(crosshairFolder.listFiles(filter));
+        lvCrosshairs.getItems().addAll(list);
+    }
+
+    @FXML
+    protected void onDeleteClick() { // TODO Delete both files
+        String fileName = lvCrosshairs.getSelectionModel().getSelectedItem();
+        // Deleting both VTF and VMT files
+        if ( new File("crosshairs/" + fileName).delete() &&  new File("crosshairs/" + removeExtension(fileName) + ".vmt").delete()) {
+            System.out.println(fileName + " has been deleted.");
+        } else {
+            System.out.println("Deletion failed for " + fileName);
+        }
+    }
+
+    public String removeExtension(String fileName) {
+        if (fileName.contains(".")) {
+            return fileName.substring(0, fileName.lastIndexOf("."));
+        } else {
+            return null;
+        }
     }
 }
